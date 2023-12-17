bb471910acc4a27a9fa5ddc23a765e39a5773f22
Clint Hoagland <c.hoagland@techsmith.com>
2023-12-17 18:03:46 -0500

    day 3, getting part candidates

--- advent-of-code-2023-tests/advent-of-code-2023-tests/Day03Tests.cs
+++ advent-of-code-2023-tests/advent-of-code-2023-tests/Day03Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using FluentAssertions;
 using NUnit.Framework;
@@ -51,6 +52,65 @@ namespace advent_of_code_2023_tests
             result[9, 1].Should().Be('6');
         }
 
+        [Test]
+        public void GetsPartCandidates_Example_GetsCorrectCandidates()
+        {
+            var exampleFilename =
+                "C:\\Projects\\Git\\advent-of-code-2023\\advent-of-code-2023-tests\\advent-of-code-2023-tests\\day03-example-input.txt";
+            var lines = File.ReadAllLines(exampleFilename);
+            var parser = new ParsesSchematic();
+            char[,] schematic = parser.Parse(lines);
+
+            var subject = new GetsPartCandidates();
+
+            List<PartCandidate> result = subject.GetPartCandidates(schematic);
+            result[0].PartNumber.Should().Be(467);
+            result[9].PartNumber.Should().Be(598);
+        }
+
+    }
+
+    public class GetsPartCandidates
+    {
+        public List<PartCandidate> GetPartCandidates(char[,] schematic)
+        {
+            List<PartCandidate> result = new List<PartCandidate>();
+            bool buildingCandidate = false;
+            string candidatePartNumber = string.Empty;
+            
+            for (int i = 0; i < schematic.GetLength(0); i++)
+            {
+                for (int j = 0; j < schematic.GetLength(1); j++)
+                {
+                    int throwaway;
+                    if (int.TryParse(schematic[i, j].ToString(), out throwaway))
+                    {
+                        buildingCandidate = true;
+                        candidatePartNumber = candidatePartNumber + schematic[i, j];
+                    }
+                    else
+                    {
+                        if (!buildingCandidate) continue;
+                        var candidate = new PartCandidate
+                        {
+                            PartNumber = int.Parse(candidatePartNumber)
+                        };
+                        result.Add(candidate);
+                            
+                        buildingCandidate = false;
+                        candidatePartNumber = String.Empty;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public class PartCandidate
+    {
+        public int PartNumber { get; set; }
+        public List<char[,]> Positions { get; set; } 
     }
 
     public class ParsesSchematic
